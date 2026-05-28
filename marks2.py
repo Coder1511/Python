8bdee614fccd41f78abcb6a97714d5ebcda9d1d0
vinayak11108@gmail.com <vinayak11108gmail.com>
2026-05-28 15:29:47 +0530

    coorected code try

--- marks2.py
+++ marks2.py
@@ -1,17 +1,18 @@
 student_name=input("Enter the student's name: ")
 subject_no=[1,2,3]
 marks=[]
-for mark in subject_no:
-    mark=int(input(f"Enter the marks of subject {subject_no}: "))
+marks_grade=[]
+for subject in subject_no:
+    mark=int(input(f"Enter the marks of subject {subject}: "))
     marks.append(mark)
     if mark<35:
-        print("The student has a grade D in subject",subject_no)
+        marks_grade.append("D")
     elif 50>mark>=35:
-        print("The student has a grade C in subject",subject_no) 
+        marks_grade.append("C")
     elif 75>mark>=50:
-        print("The student has a grade B in subject",subject_no)
+        marks_grade.append("B")
     elif 100>=mark>=75:
-        print("The student has a grade A in subject",subject_no)
+        marks_grade.append("A")    
 total_marks=sum(marks)
 average_marks=total_marks/len(marks)
 print("Student Name:",student_name)
@@ -19,6 +20,6 @@ print("Total marks:",total_marks)
 print("Average marks:",average_marks)
 print("Highest marks:",max(marks))
 print("Lowest marks:",min(marks))
-print("Grade in subject 1:",marks[0])
-print("Grade in subject 2:",marks[1])
-print("Grade in subject 3:",marks[2])
+print("Grade in subject 1:",marks_grade[0])
+print("Grade in subject 2:",marks_grade[1])
+print("Grade in subject 3:",marks_grade[2])
